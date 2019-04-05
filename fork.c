63054c269781831b616de9ca4063ea19b38a627c
Jose Medina <fermed28@gmail.com>
2019-04-05 21:55:46 +0000

    added id of each posibilities parent current and child id

--- fork.c
+++ fork.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 int main()
 {
-	if (fork() == 0)
-		printf("Hello from Child!\n");
+	pid_t pid = fork();
+	if (pid == 0)
+		printf("Hello from Child!, my PID is %u, and my parentID is %u\n", getpid(), getppid());
   	else
-		printf("Hello from Parent!\n");
+		printf("Hello from Parent!, my PID is %u, and my parentID is %u, and my child is %u\n", getpid(), getppid(), pid);
 	return (0);
 }
