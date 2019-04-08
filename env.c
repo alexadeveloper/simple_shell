798c7f55e0bc841143970f1582136a73dde070dd
Jose Medina <fermed28@gmail.com>
2019-04-08 21:51:22 +0000

    [betty]

--- env.c
+++ env.c
@@ -2,6 +2,6 @@
 extern char **environ;
 int main(int ac, char *argv[], char *envp[])
 {
-	printf("Direccion of envp: %p\n adress environ %p\n",envp, environ);
+	printf("Direccion of envp: %p\n adress environ %p\n", envp, environ);
 	return (0);
 }
