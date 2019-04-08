f33338dde74febe89f8df50da8343e4b71780730
Jose Medina <fermed28@gmail.com>
2019-04-08 21:50:30 +0000

    [added] same direction envp and global environ variable

---
+++ env.c
@@ -0,0 +1,7 @@
+#include "holberton.h"
+extern char **environ;
+int main(int ac, char *argv[], char *envp[])
+{
+	printf("Direccion of envp: %p\n adress environ %p\n",envp, environ);
+	return (0);
+}
