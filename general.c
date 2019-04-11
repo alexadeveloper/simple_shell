18e18b28cb3d1240743ecccd3fe6decb24d0dd36
Alexa <alexafdeveloper@gmail.com>
2019-04-11 17:45:54 +0000

    fixed: str_concat to basics.c

--- general.c
+++ general.c
@@ -89,38 +89,3 @@ int _strspn(const char *p, const char *s, int opt)
 	}
 	return (i);
 }
-/**
- * str_concat - Duplicate string
- * @s1: string one
- * @s2: string two
- * Return: pointer to copy array
- */
-char *str_concat(char *s1, char *s2)
-{
-	char *p;
-	unsigned int i;
-	unsigned int size_s1, size_s2;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	for (size_s1 = 0; *(s1 + size_s1) && s1; size_s1++)
-	{
-	}
-	for (size_s2 = 0; *(s2 + size_s2) && s2; size_s2++)
-	{
-	}
-	p =  malloc(sizeof(char) * (size_s1 + size_s2 + 1));
-	if (p == NULL)
-		return (NULL);
-	for (i = 0; i < size_s1 + size_s2; i++)
-	{
-		if (i < size_s1)
-			p[i] = *(s1 + i);
-		else
-			p[i] = *(s2 + i - size_s1);
-	}
-	p[i] = '\0';
-	return (p);
-}
