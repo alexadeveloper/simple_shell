084212ed0070ad7106c3409a112c9b8c8bc67e28
Alexa <alexafdeveloper@gmail.com>
2019-04-14 17:43:42 +0000

    added: function read_file

---
+++ test/main_read.c
@@ -0,0 +1,15 @@
+#include "../holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+ int main(void)
+ {
+	char *ruta;
+	
+	ruta= "./help/exit";
+
+   read_file(ruta);
+   return (0);
+ }
